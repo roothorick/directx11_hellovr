9a4941a8acae4f419a5b1f7543cdbaa9b485a8bc
JamesBear <temp_account@126.com>
2016-02-24 12:01:04 +0800

    Upgrade from dx9 to dx11

--- dxHelloworld1/dxHelloworld1.cpp
+++ dxHelloworld1/dxHelloworld1.cpp
@@ -3,9 +3,10 @@
 
 #include "stdafx.h"
 #include "dxHelloworld1.h"
-#include <d3d9.h>
+#include <d3d11.h>
+#include <DirectXColors.h>
 
-#pragma comment (lib, "d3d9.lib")
+#pragma comment (lib, "d3d11.lib")
 
 #define MAX_LOADSTRING 100
 
@@ -13,10 +14,47 @@
 HINSTANCE hInst;                                // 当前实例
 WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
 WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+UINT clientWidth = 800;
+UINT clientHeight = 600;
 
 // d3d global declarations
-LPDIRECT3D9 d3d;
-LPDIRECT3DDEVICE9 d3ddev;
+ID3D11Device*			pDevice	= nullptr;
+ID3D11DeviceContext*	pImmediateContext = nullptr;
+IDXGISwapChain*			pSwapChain = nullptr;
+ID3D11RenderTargetView*	pRenderTargetView = nullptr;
+D3D_DRIVER_TYPE			driverType;
+D3D_FEATURE_LEVEL		featureLevel;
+D3D11_VIEWPORT			viewport;
+
+namespace Memory
+{
+	template <class T> void SafeDelete(T& t)
+	{
+		if (t)
+		{
+			delete t;
+			t = nullptr;
+		}
+	}
+
+	template <class T> void SafeDeleteArr(T& t)
+	{
+		if (t)
+		{
+			delete[]t;
+			t = nullptr;
+		}
+	}
+
+	template <class T> void SafeRelease(T& t)
+	{
+		if (t)
+		{
+			t->Release();
+			t = nullptr;
+		}
+	}
+}
 
 // 此代码模块中包含的函数的前向声明: 
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -25,7 +63,7 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 // d3d function prototypes
-void initD3D(HWND hWnd);
+bool initD3D(HWND hWnd);
 void render_frame(void);
 void cleanD3D(void);
 
@@ -71,7 +109,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     return (int) msg.wParam;
 }
 
-
+void MyDebug(LPCWSTR msg)
+{
+	MessageBox(NULL, msg, _T(""), 0);
+}
 
 //
 //  函数: MyRegisterClass()
@@ -114,7 +155,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    hInst = hInstance; // 将实例句柄存储在全局变量中
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+      CW_USEDEFAULT, 0, clientWidth, clientHeight, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
@@ -200,47 +241,143 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 
 
 // this function initializes and prepares Direct3D for use
-void initD3D(HWND hWnd)
+bool initD3D(HWND hWnd)
 {
-	d3d = Direct3DCreate9(D3D_SDK_VERSION);    // create the Direct3D interface
+	UINT createDeviceFlags = 0;
 
-	D3DPRESENT_PARAMETERS d3dpp;    // create a struct to hold various device information
+#ifdef DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
 
-	ZeroMemory(&d3dpp, sizeof(d3dpp));    // clear out the struct for use
-	d3dpp.Windowed = TRUE;    // program windowed, not fullscreen
-	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;    // discard old frames
-	d3dpp.hDeviceWindow = hWnd;    // set the window to be used by Direct3D
+	// CREATE DEVICE AND SWAP CHAIN
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE, // the first thing to try, if failed, go to the next
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE
+	};
+	UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0, // texture size and others..
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+		D3D_FEATURE_LEVEL_9_3
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+	DXGI_SWAP_CHAIN_DESC swapDesc;
+	ZeroMemory(&swapDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+	swapDesc.BufferCount = 1;
+	swapDesc.BufferDesc.Width = clientWidth;
+	swapDesc.BufferDesc.Height = clientHeight;
+	swapDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // unsigned normal
+	swapDesc.BufferDesc.RefreshRate.Numerator = 60;
+	swapDesc.BufferDesc.RefreshRate.Denominator = 1;
+	swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapDesc.OutputWindow = hWnd;
+	swapDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	swapDesc.Windowed = true;
+	swapDesc.SampleDesc.Count = 1; // multisampling, which antialiasing for geometry. Turn it off
+	swapDesc.SampleDesc.Quality = 0;
+	swapDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; // alt-enter fullscreen
+
+	HRESULT errorCode;
+	for (int i = 0; i < numDriverTypes; ++i)
+	{
+		errorCode = D3D11CreateDeviceAndSwapChain(NULL, driverTypes[i], NULL, createDeviceFlags,
+			featureLevels, numFeatureLevels, D3D11_SDK_VERSION, &swapDesc, &pSwapChain, &pDevice,
+			&featureLevel, &pImmediateContext);
+
+		if (SUCCEEDED(errorCode))
+		{
+			driverType = driverTypes[i];
+			break;
+		}
+	}
+
+	if (FAILED(errorCode))
+	{
+		OutputDebugString(_T("FAILED TO CREATE DEVICE AND SWAP CHAIN"));
+		MyDebug(_T("FAILED TO CREATE DEVICE AND SWAP CHAIN"));
+		return false;
+	}
+
+	// CREATE RENDER TARGET VIEW
+	ID3D11Texture2D*	pBackBufferTex;
+	pSwapChain->GetBuffer(NULL, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&pBackBufferTex));
+	HRESULT result = pDevice->CreateRenderTargetView(pBackBufferTex, nullptr, &pRenderTargetView);
+	if (FAILED(result))
+	{
+		MyDebug(_T("ERROR"));
+	}
+
+	//BIND RENDER TARGET VIEW
+	//pImmediateContext->OMGetRenderTargets(1, &pRenderTargetView, nullptr); // I typed 'get' while I should type 'set'
+	pImmediateContext->OMSetRenderTargets(1, &pRenderTargetView, nullptr); // depth stencil view is for shadow map
+
+	//VIEWPORT CREATION
+	viewport.Width = static_cast<float>(clientWidth);
+	viewport.Height = static_cast<float>(clientHeight);
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+
+	// BIND VIEWPORT
+	pImmediateContext->RSSetViewports(1, &viewport);
+	return true;
+
+	//d3d = Direct3DCreate9(D3D_SDK_VERSION);    // create the Direct3D interface
+
+	//D3DPRESENT_PARAMETERS d3dpp;    // create a struct to hold various device information
+
+	//ZeroMemory(&d3dpp, sizeof(d3dpp));    // clear out the struct for use
+	//d3dpp.Windowed = TRUE;    // program windowed, not fullscreen
+	//d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;    // discard old frames
+	//d3dpp.hDeviceWindow = hWnd;    // set the window to be used by Direct3D
 
 
-								   // create a device class using this information and the info from the d3dpp stuct
-	d3d->CreateDevice(D3DADAPTER_DEFAULT,
-		D3DDEVTYPE_HAL,
-		hWnd,
-		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-		&d3dpp,
-		&d3ddev);
+	//							   // create a device class using this information and the info from the d3dpp stuct
+	//d3d->CreateDevice(D3DADAPTER_DEFAULT,
+	//	D3DDEVTYPE_HAL,
+	//	hWnd,
+	//	D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+	//	&d3dpp,
+	//	&d3ddev);
 }
 
 
 // this is the function used to render a single frame
 void render_frame(void)
 {
-	// clear the window to a deep blue
-	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
+	pImmediateContext->ClearRenderTargetView(pRenderTargetView, DirectX::Colors::CornflowerBlue);
 
-	d3ddev->BeginScene();    // begins the 3D scene
+	pSwapChain->Present(0, 0);
 
-							 // do 3D rendering on the back buffer here
+	//// clear the window to a deep blue
+	//d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
 
-	d3ddev->EndScene();    // ends the 3D scene
+	//d3ddev->BeginScene();    // begins the 3D scene
 
-	d3ddev->Present(NULL, NULL, NULL, NULL);   // displays the created frame on the screen
+	//						 // do 3D rendering on the back buffer here
+
+	//d3ddev->EndScene();    // ends the 3D scene
+
+	//d3ddev->Present(NULL, NULL, NULL, NULL);   // displays the created frame on the screen
 }
 
 
 // this is the function that cleans up Direct3D and COM
 void cleanD3D(void)
 {
-	d3ddev->Release();    // close and release the 3D device
-	d3d->Release();    // close and release Direct3D
+	if (pImmediateContext) pImmediateContext->ClearState();
+	Memory::SafeRelease(pRenderTargetView);
+	Memory::SafeRelease(pSwapChain);
+	Memory::SafeRelease(pImmediateContext);
+	Memory::SafeRelease(pDevice);
+
+	//d3ddev->Release();    // close and release the 3D device
+	//d3d->Release();    // close and release Direct3D
 }
